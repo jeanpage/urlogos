71795b57de385cb313f3c7cc2621ea743e35984a
Jean Page <jpage@JeanPageMac.local>
2017-03-01 16:22:27 -0600

    adding template and css for splash page

---
+++ wp-content/themes/urlogos/template-full-splash.php
@@ -0,0 +1,11 @@
+<?php
+	/*  Template Name: Full Width  */
+	get_header();
+	$container_classes = 'single';
+?>
+<section>
+<div class="column green">
+  <img src="http://res.cloudinary.com/urlogos/image/upload/PIL_7960.jpg">
+</div>
+</section>  
+<?php get_footer();?>
