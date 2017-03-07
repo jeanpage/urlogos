4a2e0271d478c4ea1ff16980b84a8db299be2a72
Jean Page <jpage@JeanPageMac.local>
2017-03-07 15:58:38 -0600

    adding grid to splash type

--- wp-content/themes/urlogos/template-full-splash-bg.php
+++ wp-content/themes/urlogos/template-full-splash-bg.php
@@ -3,10 +3,12 @@
 	get_header();
 	$container_classes = 'single';?>
 <section>
+
+<h1 class="splashType">Full<span style="float:right;">Stack</span><br> Designer/<br>Brand<span style="float:right;"> Developer</span></h1>
+
 <div class="column green">
 <!--adding background image to css -->
   <div class="bg-img"></div>
-
 </div>
 </section>  
 
