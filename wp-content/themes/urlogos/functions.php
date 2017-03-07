95bc3931a80a9d6f3eb6856ab4f277d34b8b663f
Jean Page <jpage@JeanPageMac.local>
2017-03-06 18:54:12 -0600

    fix for background image

--- wp-content/themes/urlogos/functions.php
+++ wp-content/themes/urlogos/functions.php
@@ -2,7 +2,7 @@
 
 function urlogos_enqueue_styles() {
 
-    $parent_style = 'parent-style'; // This is 'twentyfifteen-style' for the Twenty Fifteen theme.
+    $parent_style = 'parent-style'; 
 
     wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
     wp_enqueue_style( 'child-style',
--- wp-content/themes/urlogos/template-full-splash.php
+++ wp-content/themes/urlogos/template-full-splash.php
@@ -1,8 +1,7 @@
 <?php
 	/*  Template Name: Splash  */
 	get_header();
-	$container_classes = 'single';
-?>
+	$container_classes = 'single';?>
 <section>
 <div class="column green">
   <img src="http://res.cloudinary.com/urlogos/image/upload/PIL_7960.jpg">
