9d37696f48baed2dedadd3a82d25594f6212564b
Jean Page <jpage@JeanPageMac.local>
2017-03-07 11:32:37 -0600

    new file for splash

---
+++ wp-content/themes/urlogos/template-full-splash-bg.php
@@ -0,0 +1,13 @@
+<?php
+	/*  Template Name: Splash 2  */
+	get_header();
+	$container_classes = 'single';?>
+<section>
+<div class="column green">
+<!--adding background image to css -->
+  <div class="bg-img">
+
+</div>
+</section>  
+
+<?php get_footer();?>
