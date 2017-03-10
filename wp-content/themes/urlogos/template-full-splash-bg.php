dc61a104116a75af8bf1050465f594fc25338eec
Jean Page <jpage@JeanPageMac.local>
2017-03-09 18:43:48 -0600

    splash page dom switch

--- wp-content/themes/urlogos/template-full-splash-bg.php
+++ wp-content/themes/urlogos/template-full-splash-bg.php
@@ -4,12 +4,17 @@
 	$container_classes = 'single';?>
 <section>
 
-<h1 class="splashType">Full<span style="float:right;">Stack</span><br> Designer/<br>Brand<span style="float:right;"> Developer</span></h1>
-
 <div class="column green">
-<!--adding background image to css -->
-  <div class="bg-img"></div>
+	<div class="bg-img">
+ 		<h1 class="splashType">Full
+ 		<span style="float:right;">Stack</span><br> 
+ 		Designer/<br>
+ 		Brand
+ 		<span style="float:right;"> Developer</span></h1>
+
+ 	</div>
 </div>
+
 </section>  
 
 <?php get_footer();?>
