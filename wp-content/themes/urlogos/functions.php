554ff02e176c0545a2910eed44e1b2cf5cd7d062
Jean Page <jpage@JeanPageMac.local>
2017-03-29 14:58:02 -0500

    version number fix

--- wp-content/themes/urlogos/functions.php
+++ wp-content/themes/urlogos/functions.php
@@ -1,14 +1,12 @@
 <?php 
 
-
+add_action( 'wp_enqueue_scripts', 'urlogos_enqueue_styles' );
 
 function urlogos_enqueue_styles() {
     $parent_style = 'parent-style'; 
     wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
-    wp_enqueue_style( 'child-style', get_stylesheet_directory	_uri() . '/style.css', array( $parent_style ));
+    wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/style.css', array( $parent_style ), wp_get_theme()->get('Version = false'));
 	}
-
-add_action( 'wp_enqueue_scripts', 'urlogos_enqueue_styles' );
 
 if ( ! isset( $content_width ) ) {
 $content_width = 1200; }
