6603d51a2ea6376b2641e08493dd2e5c138d1310
Jean Page <jpage@JeanPageMac.local>
2017-03-29 16:10:37 -0500

    adding comments and org to functions

--- wp-content/themes/urlogos/functions.php
+++ wp-content/themes/urlogos/functions.php
@@ -1,5 +1,5 @@
 <?php 
-// Child Styles	
+// Function to add child theme
 add_action( 	'wp_enqueue_scripts', 'urlogos_enqueue_styles' );
 // Remove WP Version From Styles	
 add_filter( 'style_loader_src', 'sdt_remove_ver_css_js', 9999 );
@@ -13,7 +13,6 @@ function urlogos_enqueue_styles() {
     wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
 	wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/style.css', array( $parent_style ), false);
 	}
-
 
 // Function to remove version numbers
 function sdt_remove_ver_css_js( $src ) {
