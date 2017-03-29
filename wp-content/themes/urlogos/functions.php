f7892fe5bba891038eadd1539aec33c04c8963ea
Jean Page <jpage@JeanPageMac.local>
2017-03-29 14:44:01 -0500

    Take out version parameter from CSS child theme

--- wp-content/themes/urlogos/functions.php
+++ wp-content/themes/urlogos/functions.php
@@ -5,7 +5,7 @@
 function urlogos_enqueue_styles() {
     $parent_style = 'parent-style'; 
     wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
-    wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/style.css', array( $parent_style ), wp_get_theme()->get('Version'));
+    wp_enqueue_style( 'child-style', get_stylesheet_directory	_uri() . '/style.css', array( $parent_style ));
 	}
 
 add_action( 'wp_enqueue_scripts', 'urlogos_enqueue_styles' );
